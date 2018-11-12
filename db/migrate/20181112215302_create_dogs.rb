861c01f3fb1829f310f6485763dfdd94fb087e31
Justin Kenney <justinkenney13@gmail.com>
2018-11-12 21:55:17 +0000

    automatically backed up by learn

--- db/migrate/20181112215302_create_dogs.rb
+++ db/migrate/20181112215302_create_dogs.rb
@@ -1,13 +1,15 @@
 class CreateDogs < ActiveRecord::Migration
+  
   def up
     create_table :dogs |t| do
       t.string :name
       t.string :breed
     end
-
-    def down
-      drop_table :dogs
-    end
-    
   end
+
+  def down
+    drop_table :dogs
+  end
+  
+  
 end
